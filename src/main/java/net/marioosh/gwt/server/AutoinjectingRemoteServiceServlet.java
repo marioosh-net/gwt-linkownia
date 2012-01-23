4f4a52e559bf57d2530bf7bd11f13b85d9bc927a
marioosh <marioosh.net@gmail.com>
2012-01-23 14:10:03 +0100

    first commit

---
+++ src/main/java/net/marioosh/gwt/server/AutoinjectingRemoteServiceServlet.java
@@ -0,0 +1,24 @@
+package net.marioosh.gwt.server;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * http://pgt.de/2009/07/17/non-invasive-gwt-and-spring-integration-reloaded/
+ */
+@SuppressWarnings("serial")
+public class AutoinjectingRemoteServiceServlet extends RemoteServiceServlet {
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(config.getServletContext());
+		AutowireCapableBeanFactory beanFactory = ctx.getAutowireCapableBeanFactory();
+		beanFactory.autowireBean(this);
+	}
+
+}
